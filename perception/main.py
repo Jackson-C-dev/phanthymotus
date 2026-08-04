77a6041ec0a8816ae0932df31b743dba694438f2
Jackson-C-dev <chq8124@gmail.com>
2026-08-04 17:18:40 +0800

    Load ObstacleDistancePlugin if enabled in config

--- perception/main.py
+++ perception/main.py
@@ -97,6 +97,11 @@ class PerceptionBundle:
             self._plugins.append(OCRPlugin(plugins_cfg["ocr"], executor))
             log.info("OCRPlugin loaded")
 
+        if plugins_cfg.get("obstacle", {}).get("enabled", False):
+            from plugins.obstacle import ObstacleDistancePlugin
+            self._plugins.append(ObstacleDistancePlugin(plugins_cfg["obstacle"], executor))
+            log.info("ObstacleDistancePlugin loaded")
+
     def get_all_tools(self) -> list:
         tools = []
         for p in self._plugins:
@@ -450,3 +455,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
